102f72b72be04731ebd674d7fedc05b644ce994b
endahsukma <37181273+endahsukma@users.noreply.github.com>
2018-05-21 09:49:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include_once ('template_atas.php'); ?>
+
+<body>
+   <form method="post" action="login_proses.php">
+      <h1>LOGIN USER</h1>
+      <input placeholder="Username" type="text"/> <br/>
+      <input placeholder="Password" type="password"/> <br/>
+      <button class="btn">Log in</button> <br/> <br/> <br/> <br/> <br/> <br/> <br/> 
+   </form> 
+</body>
+
+<?php include_once ('template_bawah.php') ; ?>
+
