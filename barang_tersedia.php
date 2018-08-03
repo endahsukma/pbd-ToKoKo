e67747c960a6037208536578eda8d57e7a240126
endahsukma <37181273+endahsukma@users.noreply.github.com>
2018-08-03 07:24:45 +0700

    Update barang_tersedia.php

--- barang_tersedia.php
+++ barang_tersedia.php
@@ -1,3 +1,9 @@
+<html lang="en">
+<style>
+body {
+background-image : url("g2.jpg");
+}
+</style>
 <?php
 	$barang_pilih = 0;
 if(isset($_COOKIE['keranjang'])){
@@ -10,35 +16,35 @@ setcookie('keranjang',$barang_pilih,time()+3600);
 }
 include "koneksi.php";
 $sql = "select * from barang where idbarang not in (".$barang_pilih.")
-		and stok > 0 order by idbarang desc";
+		and ks > 0 order by idbarang desc";
 $hasil = mysqli_query($kon,$sql);
 if (!$hasil)
 	die ("Gagal query..".mysql_error());
 	?>
-<h2> DAFTAR BARANG TERSEDIA </h2>
+	<center/>
+<h2> DAFTAR BELANJAAN </h2>
+
 <table border="1">
 	<tr>
-	<th>Foto</th>
+	<th>Gambar</th>
 	<th>Nama Barang</th>
-	<th>Harga jual</th>
-	<th>Stok</th>
-	<th>Operasi</th>
+	<th>Harga Barang</th>
+	<th>Kode Barang</th>
 	</tr>
 	<?php
 		$no = 0;
 		while ($row = mysqli_fetch_assoc($hasil)) {
 			echo "<tr>";
-			echo  "<td> <a href='pict/{$row['foto']}'/>
-					<img src='thumb/t_{$row['foto']} 'width='100' />
+			echo"<td> <a href='pict/{$row['foto']}'/>
+					<img src='thumb/t_{$row['foto']}' width='100' />
 					</a> </td>";
 			echo "<td>".$row['nama']."</td>";
 			echo "<td>".$row['harga']."</td>";
-			echo "<td>".$row['stok']."</td>";
+			echo "<td>".$row['ks']."</td>";
 			echo "<td>";
-			echo " <a href='".$_SERVER['PHP_SELF']."?idbarang=".
-				$row['idbarang']."'>BELI </a>";
-			echo "</td>";
 			echo "</tr>";
 			}
 	?>
 </table>
+
+
