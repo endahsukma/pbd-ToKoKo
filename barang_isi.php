bf36392e48a719301cb7a57dfc2bee2bf9877e1e
endahsukma <37181273+endahsukma@users.noreply.github.com>
2018-08-03 07:26:28 +0700

    Update barang_isi.php

--- barang_isi.php
+++ barang_isi.php
@@ -1,21 +1,33 @@
-<h2>.:: ISI BARANG ::.</h2>
+<html lang="en">
+<style>
+body {
+background-image : url("g1.jpg");
+}
+</style>
+<h2>PERSEDIAAN SKIN CARE</h2>
 	<form action="barang_simpan.php" method="post" enctype="multipart/form-data">
+	<input type="hiden" name"idbarang" values="<?php echo $idbarang;?>" />
 	<table border="1">
 		<tr>
 		<td>Nama Barang</td>
-		<td><input type="text" name="nama" /></tr>
+		<td><input type="text" name="nama" values="<?php echo $nama;?>" /></tr>
 		</tr>
 		<tr>
 		<td>Harga jual</td>
-		<td><input type="text" name="harga" /></tr>
+		<td><input type="text" name="harga" values="<?php echo $harga;?>" /></tr>
 		</tr>
 		<tr>
-		<td>Stok</td>
-		<td><input type="text" name="stok" /></tr>
+		<td>Kode</td>
+		<td><input type="text" name="ks" values="<?php echo $ks;?>" /></tr>
 		</tr>
 		<tr>
-		<td>Gambar [max=1.5MB]</td>
+		<td>Gambar [max=2.5MB]</td>
+		<td>
+		<input type="file" name-"foto">
+		<input type="hiden" name"foto_lama" values="<?php echo $foto;?>" /> <br/>
+		<img src="<?php echo "thumb/t_".$foto; ?>" width="100px" />
 		<td><input type="file" name="foto"></td>
+		</td>
 		</tr>
 		<tr>
 		<td colspan="2" align="center">
